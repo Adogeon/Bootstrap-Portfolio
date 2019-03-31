f8d69e7a836fb869f74b048d7b9968009ff1c4d7
Anh Dao <addao823@gmail.com>
2019-03-30 19:19:29 -0700

    Add Link to Gif-pulling

---
+++ asset/javascript/app.js
@@ -0,0 +1,6 @@
+
+document.querySelector("#gif-pulling").addEventListener("click", function() {
+    console.log("Click")
+    var newUrl = "https://adogeon.github.io/Giphy-pulling/"
+    document.location.href = newUrl;
+})
